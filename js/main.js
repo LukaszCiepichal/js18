bde5491cff39811fd9a4541298add7e1fa01b86d
lukasz ciepichal <lukaszciepichal1@gmail.com>
2021-11-08 11:47:51 +0100

    poprawka funkcji

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-let num1 = 7
-let num2 = 2
 
 
-console.log('Wynik dodawania wynosi', num1+num2)
-console.log('Wynik odejmowania wynosi', num1-num2)
-console.log('Wynik mnożenia wynosi', num1*num2)
+// console.log('Wynik dodawania wynosi', num1+num2)
+// console.log('Wynik odejmowania wynosi', num1-num2)
+// console.log('Wynik mnożenia wynosi', num1*num2)
+
+function dzialanie(liczba1, liczba2){
+    let wynikDodawania = liczba1 + liczba2;
+    console.log('Wynik dodawania wynosi ' + wynikDodawania);
+    
+    let wynikOdejmowania = liczba1 - liczba2;
+    console.log('Wynik odejmowania wynosi ' + wynikOdejmowania)
+
+    let wynikMnozenia = liczba1 * liczba2;
+    console.log('Wynik jest nieprawidłowy ')
+    
+    
+}
+
+dzialanie(2, -1);
